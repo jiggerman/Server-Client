e8af795939e54d48f68e716be3d1de659ace619d
Liggerman <liggerman@mail.ru>
2023-11-28 23:49:24 +0300

    Multi-stream communication, client -> servet

--- client.py
+++ client.py
@@ -8,3 +8,25 @@ ADDRES = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDRES)
+
+
+def send_message(msg):
+    message = msg.encode(FORMAT)
+    client.send(message)
+
+
+def start():
+    while True:
+        try:
+            message = input()
+
+            if message != DISCONNECT_MESSAGE:
+                send_message(message)
+            else:
+                break
+        except:
+            print(f"Connection is broken!")
+
+
+if __name__ == '__main__':
+    start()
